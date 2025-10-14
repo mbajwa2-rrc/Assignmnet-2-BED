a25d0ab76da9c03ef19c906c4451c973c307e646
mbajwa2-rrc <mbajwa2@academic.rrc.ca>
2025-10-13 20:37:00 -0500

    Edited tsconfig.json file

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -1,13 +1,10 @@
 import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
 import { getFirestore, Firestore } from "firebase-admin/firestore";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+import serviceAccount from "../serviceAccountKey.json" assert { type: "json" };
 
-// Initialize Firebase app using the service account
 initializeApp({
   credential: cert(serviceAccount as ServiceAccount),
 });
 
-// Get a Firestore reference
 const db: Firestore = getFirestore();
-
 export { db };
