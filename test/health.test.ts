ba2fbdd997f374ba88f30f62bb441b03477e2dd3
mbajwa2-rrc <mbajwa2@academic.rrc.ca>
2025-10-30 15:49:41 -0500

    Edited serviceAccountKey.json file

--- test/health.test.ts
+++ test/health.test.ts
@@ -1,14 +1,16 @@
 import request from "supertest";
 import app from "../src/app";
 
+jest.setTimeout(10000);
+
 describe("GET /api/v1/health", () => {
-    it("should return 200 OK and health details", async () => {
-        const response = await request(app).get("/api/v1/health");
-        
-        expect(response.status).toBe(200);
-        expect(response.body).toHaveProperty("status", "OK");
-        expect(response.body).toHaveProperty("uptime");
-        expect(response.body).toHaveProperty("timestamp");
-        expect(response.body).toHaveProperty("version", "1.0.0");
-    });
+  it("should return 200 OK and health details", async () => {
+    const response = await request(app).get("/api/v1/health");
+
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty("status", "OK");
+    expect(response.body).toHaveProperty("uptime");
+    expect(response.body).toHaveProperty("timestamp");
+    expect(response.body).toHaveProperty("version", "1.0.0");
+  });
 });
